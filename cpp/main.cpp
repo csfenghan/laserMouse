f23a4ce6922ff509d79b3c789598844d4e058542
fenghan <csfenghan@outlook.com>
2021-08-01 16:45:37 +0800

    更新了.gitignore

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,10 +6,7 @@ using namespace std;
 int main(int argc, char **argv) {
 	lasermouse::Calibrater calibrater = lasermouse::Calibrater(1080, 1920);
 
-	cv::Mat img = calibrater.createChessboard();
+	auto cap = cv::VideoCapture(0);
 
-	printf("%d, %d", img.rows, img.cols);
-	cv::imshow("img", img);
-	if (cv::waitKey(0) == 'q')
-		return 0;
+	calibrater.calibrate(cap);
 }
