f4fd0d6ea865810d899fae9a54c29015ace5a1ef
fenghan <csfenghan@outlook.com>
2021-08-01 21:36:04 +0800

    linux下c++版本的鼠标控制基本完成

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,18 @@
-#include "calibrate.hpp"
-#include "detect.hpp"
+#include "location.hpp"
 #include "mouse_control.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    
+    lasermouse::Location location; 
+    lasermouse::MouseControl mouse_control;
+
+    int x = 0, y = 0;
+    location.calibrate();
+    while (true) {
+        if (location.position(x, y)){
+            mouse_control.moveTo(x, y);
+        }
+    }
 }
