e34b64aaf35eeffcaf4a15f34bcf23c9a05fc379
fenghan <csfenghan@outlook.com>
2021-08-01 20:58:40 +0800

    删除了追踪功能

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,13 +1,10 @@
 #include "calibrate.hpp"
 #include "detect.hpp"
+#include "mouse_control.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    auto calibrater = lasermouse::Calibrater(1080, 1920);
-    calibrater.test(0);
-
-    //auto detector = lasermouse::Detector();
-    //detector.test(0);
+    
 }
