36bb70e070f1d5eba903585bb5448f917e1568cb
fenghan <csfenghan@outlook.com>
2021-08-01 16:04:29 +0800

    添加了显示棋盘图片的代码

--- cpp/calibrate.cpp
+++ cpp/calibrate.cpp
@@ -1,4 +1,5 @@
 #include "calibrate.hpp"
+#include <iostream>
 
 namespace lasermouse{
 Calibrater::Calibrater(int height, int width, int chessboard_cols, int chessboard_rows):
@@ -6,25 +7,28 @@ Calibrater::Calibrater(int height, int width, int chessboard_cols, int chessboar
 
 cv::Mat Calibrater::createChessboard() {
 	int n_pix = MIN(height_, width_) / 10;	// 一个块的像素数
-	int col = n_pix * (chessboard_cols_ + 1);
-	int row = n_pix *(chessboard_rows_ + 1);
-	
-	// 创建棋盘图片
-	cv::Mat result = cv::Mat(row, col, CV_8UC1, cv::Scalar::all(0));
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			if (((i / n_pix) & 0x01) == 0) {	// 如果是偶数行
-				if (((j / n_pix) & 0x01) != 0) {
-					result.at<uchar>(i, j) = 255;
+	int pad_h = height_ - n_pix * (chessboard_rows_+ 1);	
+	int pad_w = width_ - n_pix * (chessboard_cols_+ 1);
+
+	int row_start = pad_h / 2, row_end = height_ - (pad_h - pad_h / 2);	
+	int col_start = pad_w / 2, col_end = width_ - (pad_w - pad_w / 2);
+
+	// 在中心位置创建棋盘图片
+	cv::Mat result = cv::Mat(height_, width_, CV_8UC1, cv::Scalar::all(255));
+	for (int i = row_start; i < row_end; i++) {
+		for (int j = col_start; j < col_end; j++) {
+			if ((((i - row_start) / n_pix) & 0x01) == 0) {	// 如果是偶数行
+				if ((((j - col_start) / n_pix) & 0x01) != 0) {
+					result.at<uchar>(i, j) = 0;
 				}
 			} else { 	// 如果是奇数行
-				if (((j / n_pix) & 0x01) == 0) {
-					result.at<uchar>(i, j) = 255;
+				if ((((j - col_start) / n_pix) & 0x01) == 0) {
+					result.at<uchar>(i, j) = 0;
 				}
 			}
 		}
 	}
-	
+
 	return result;
 }
 }
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,5 +4,12 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+	lasermouse::Calibrater calibrater = lasermouse::Calibrater(1080, 1920);
 
+	cv::Mat img = calibrater.createChessboard();
+
+	printf("%d, %d", img.rows, img.cols);
+	cv::imshow("img", img);
+	if (cv::waitKey(0) == 'q')
+		return 0;
 }
